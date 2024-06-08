97bd6b48983978da07baf2f9f5cbea6cf0041e8b
ValeMQ <144612009+ValeMQ@users.noreply.github.com>
2024-06-07 23:52:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+document.addEventListener('keydown', function (event) {
+    movePacman(event.key);
+});
+
+function movePacman(key) {
+    var pacman1 = document.getElementById('pacman1');
+    var pacman2 = document.getElementById('pacman2');
+    var step = 50;
+
+    switch (key) {
+        case 'ArrowUp':
+            break;
+        case 'ArrowDown':
+            break;
+        case 'ArrowLeft':
+            break;
+        case 'ArrowRight':
+            break;
+    }
+}
